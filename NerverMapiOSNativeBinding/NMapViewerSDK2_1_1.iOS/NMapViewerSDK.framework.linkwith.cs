52dd1198c6bf3ccbbd71b9b0d68abe8d7cc8799d
윤창 허 <imagef2@gmail.com>
2018-03-28 14:38:58 +0900

    주석 처리

--- NerverMapiOSNativeBinding/NMapViewerSDK2_1_1.iOS/NMapViewerSDK.framework.linkwith.cs
+++ NerverMapiOSNativeBinding/NMapViewerSDK2_1_1.iOS/NMapViewerSDK.framework.linkwith.cs
@@ -1,7 +1,7 @@
 ﻿using ObjCRuntime;
 
 [assembly: LinkWith("NMapViewerSDK.framework",
-                    Frameworks = "Foundation CoreGraphics CoreLocation QuartzCore UIKit",
+                    //Frameworks = "Foundation CoreGraphics CoreLocation QuartzCore UIKit",
                     SmartLink = true,
                     ForceLoad = true,
                     LinkerFlags = "-ObjC -lxml2")]
